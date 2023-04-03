f39f66ed886a725a85aaca1d3ac698bfa8a259c5
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-04-03 18:44:45 +0200

    Remove duplicated directory

--- prefect/prefect_blocks/gcp_credentials_blocks.py
+++
@@ -1,9 +0,0 @@
-from prefect_gcp.credentials import GcpCredentials
-
-gcs_credentials_block = GcpCredentials(
-    service_account_file="../../.secrets/gcs_credentials_key.json"
-)
-
-gcs_credentials_block.save("gcs-credentials", overwrite=True)
-
-
