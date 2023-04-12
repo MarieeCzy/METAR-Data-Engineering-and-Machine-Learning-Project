c30267c2bdfda6a8e6b06be691072af0529dae26
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-04-12 20:24:56 +0200

    Ref: Add arg parser for deployments
    - create parquet deployment loacl to gcs
    - rename local-to-gcs-parquet-loader.py to
    local_to_gcs_parquet_loader.py

--- prefect_orchestration/deployments/deployments_config.py
+++ prefect_orchestration/deployments/deployments_config.py
@@ -1,9 +1,10 @@
 '''
-This script builds and applies a Prefect deployment for 
-downloading web batch data and saving it locally.
+This script builds and applies a Prefect deployments for 
+downloading web batch data and saving it locally and to GCS.
 '''
 
 from flows.batch_data_downloader import extract_stations_and_transfer_to_save
+from flows.local_to_gcs_parquet_loader import local_to_gcs
 from prefect.deployments import Deployment
 
 save_data_locally_deployment = Deployment.build_from_flow(
@@ -12,7 +13,13 @@ save_data_locally_deployment = Deployment.build_from_flow(
     work_queue_name="default",
 )
 
+move_data_to_gcs_deployment = Deployment.build_from_flow(
+    flow=local_to_gcs,
+    name="Get path to file and save to GCS bucket",
+    work_queue_name="default",
+)
+
 if __name__ == "__main__":
     save_data_locally_deployment.apply()
-    
+    move_data_to_gcs_deployment.apply()
 
--- prefect_orchestration/deployments/deployments_run.py
+++ prefect_orchestration/deployments/deployments_run.py
@@ -5,12 +5,25 @@ The main() function runs the deployment using the run_deployment() function
 from the Prefect deployments module. 
 The response from the deployment is then printed to the console.
 '''
-
+import argparse
 from prefect.deployments import run_deployment
 
-def main() -> None:
-    response = run_deployment("Flow extracting stations from a given network/Download web batch data and save it locally")
+def main(args) -> None:
+    stage = args.stage
+    
+    if stage == 'S1':
+        print('Web -> local') 
+        response = run_deployment("Flow extracting stations from a given network/Download web batch data and save it locally")
+        
+    elif stage == 'S2':
+        print('Local -> GCS') 
+        response = run_deployment("Load local parquet file to GCS bucket/Get path to file and save to GCS bucket")
+    
     print(response)
     
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Manage data pipeline deployments")
+    parser.add_argument('--stage', required=True, help='"S1" for WEB TO LOCAL, "S2" for LOCAL TO GCS')
+    
+    args = parser.parse_args()
+    main(args)
