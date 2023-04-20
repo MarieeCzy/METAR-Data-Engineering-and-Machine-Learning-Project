ac3e6a846207fee89f5b3b5f96b0eca411c2e244
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-04-20 23:45:43 +0200

    Ref:Change absolute paths variables to directories

--- prefect_orchestration/deployments/flows/config.py
+++ prefect_orchestration/deployments/flows/config.py
@@ -1,13 +1,21 @@
 '''
 Configuration module 
 '''
+from os import environ
+from pathlib import Path
+import os
 
-flows_absolute_path = '/Users/mariaczapczyk/Documents/GitHub/METAR-Data-Engineering-and-Machine-Learning-Project/prefect_orchestration/deployments/flows'
+#path to root project directory
+ROOT_DIR_PATH = Path(__file__).parent.parent.parent.parent
+DATA_RELATIVE_PATH = 'prefect_orchestration/deployments/flows/data'
 
 #networks_list = ['FR__ASOS','HU__ASOS','PL__ASOS','DE__ASOS','ES__ASOS','GR__ASOS','IT__ASOS','AT__ASOS','GB__ASOS']
-networks_list = ['HU__ASOS','PL__ASOS']
+networks_list = ['HU__ASOS','PL__ASOS', 'IT__ASOS']
 
 start_year = 2023
 start_month = 1
 start_day = 1
+
+
+batch_bucket_name = "batch-metar-bucket-v1"
 
