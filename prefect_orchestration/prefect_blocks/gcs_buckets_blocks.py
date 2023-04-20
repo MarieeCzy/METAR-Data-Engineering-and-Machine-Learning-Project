315c7ed4d6d9993b8100bb3e77377d1fd66c886d
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-04-20 23:39:34 +0200

    Ref: Add reference to config module

--- prefect_orchestration/prefect_blocks/gcs_buckets_blocks.py
+++ prefect_orchestration/prefect_blocks/gcs_buckets_blocks.py
@@ -1,10 +1,15 @@
+import sys
+sys.path.append('..')
+
+from deployments.flows import config
 from prefect_gcp.cloud_storage import GcsBucket
 from prefect_gcp.credentials import GcpCredentials
 
 gcs_batch_bucket_block = GcsBucket(
     gcp_credentials=GcpCredentials.load("google-cloud-storage-credentials-metar"),
-    #insert your GCS batch data bucket name below
-    bucket = "batch-metar-bucket" 
+    
+    #insert your GCS batch data bucket name in ../deployments/flows/config module
+    bucket = config.batch_bucket_name
     )
 
 gcs_batch_bucket_block.save("google-cloud-storage-bucket-block-metar", overwrite=True)
