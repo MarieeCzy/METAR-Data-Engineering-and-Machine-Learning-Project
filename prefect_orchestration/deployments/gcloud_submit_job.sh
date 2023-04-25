7f786b2a62abd0420866855c8b6c75f34373c3b8
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-04-25 20:10:35 +0200

    Feat: Add temporary bucket

--- prefect_orchestration/deployments/gcloud_submit_job.sh
+++ prefect_orchestration/deployments/gcloud_submit_job.sh
@@ -1,11 +1,12 @@
 gcloud dataproc jobs submit pyspark \
-    --cluster=metar-dataproc-cluster-v2 \
+    --cluster=metar-project-test \
     --region=europe-west1 \
     --jars=gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar \
-    gs://batch-metar-bucket-v2/code/pyspark_sql.py \
+    gs://code/pyspark_sql.py \
     -- \
-        --input=gs://batch-metar-bucket-v2/data/PL__ASOS/*/* \
-        --bq_output=reports.PL__ASOS
+        --input=gs://batch-metar-bucket-1/data/ES__ASOS/*/* \
+        --bq_output=reports.ES__ASOS \
+        --temp_bucket=dataproc-temp-europe-west1-204246137998-xrofmh5o
 
 #chmod +x submit_spark_job.sh
 #./submit_spark_job.sh
