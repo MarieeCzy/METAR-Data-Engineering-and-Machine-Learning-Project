b70009dc5d9e0b5894c02b581dd886c29fe040e3
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-04-25 20:10:35 +0200

    Feat: Update config.py module

--- prefect_orchestration/deployments/flows/config.py
+++ prefect_orchestration/deployments/flows/config.py
@@ -1,21 +1,15 @@
 '''
 Configuration module 
 '''
-from os import environ
-from pathlib import Path
-import os
-
-#path to root project directory
-ROOT_DIR_PATH = Path(__file__)
-DATA_RELATIVE_PATH = 'prefect_orchestration/deployments/flows/data'
 
 #networks_list = ['FR__ASOS','HU__ASOS','PL__ASOS','DE__ASOS','ES__ASOS','GR__ASOS','IT__ASOS','AT__ASOS','GB__ASOS']
-networks_list = ['HU__ASOS','PL__ASOS', 'IT__ASOS']
+networks_list = ['PL__ASOS']
 
-start_year = 2023
+start_year = 1990
 start_month = 1
 start_day = 1
 
 
-batch_bucket_name = "batch-metar-bucket-v1"
+batch_bucket_name = "batch-metar-bucket-v2"
+
 
