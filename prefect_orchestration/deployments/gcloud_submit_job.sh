23c5204c1a3a97ade5fc31788fa7822272d6ef36
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-05-19 11:54:10 +0200

    Ref: Add sql_queries_config.yaml file

--- prefect_orchestration/deployments/gcloud_submit_job.sh
+++ prefect_orchestration/deployments/gcloud_submit_job.sh
@@ -1,7 +1,8 @@
 gcloud dataproc jobs submit pyspark \
     --cluster=metar-cluster \
-    --region=europe-west1 \
+    --region=europe-west2 \
     --jars=gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar \
+    --files=gs://code-metar-bucket-2/code/sql_queries_config.yaml\
     gs://code-metar-bucket-2/code/pyspark_sql.py \
     -- \
         --input=gs://batch-metar-bucket-2/data/ES__ASOS/*/* \
