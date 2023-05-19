901e76544219f0223c367c2175de25b96a6002b3
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-05-19 10:33:44 +0200

    Ref: Create Class to handle data write jobs
    - class DataTransformationManager
    - class SparkManager

--- prefect_orchestration/deployments/flows/pyspark_sql.py
+++ prefect_orchestration/deployments/flows/pyspark_sql.py
@@ -19,18 +19,41 @@ input = args.input
 bq_output = args.bq_output
 temp_bucket = args.temp_bucket
 
-conf = SparkConf()\
-    .set("spark.sql.parquet.writeLegacyFormat","true")\
-    .set("spark.sql.files.ignoreCorruptFiles", "true")\
-    .set("spark.sql.debug.maxToStringFields",1000)
 
-sc = SparkContext(conf=conf)
+class SparkManager:
+    def __init__(self,
+                 temp_bucket) -> None:
 
-spark = SparkSession.builder \
-    .appName('metar-project') \
-    .config(conf=sc.getConf())\
-    .getOrCreate()
-    
+        self.temp_bucket = temp_bucket
+        self.spark_conf = self.spark_configuration()
+        self.spark_ctx = self.spark_context()
+        self.spark = self.spark_sesion_builder()
+
+    def spark_configuration(self):
+        conf = SparkConf()\
+            .set("spark.sql.parquet.writeLegacyFormat", "true")\
+            .set("spark.sql.files.ignoreCorruptFiles", "true")\
+            .set("spark.sql.debug.maxToStringFields", 1000)\
+            .set('temporaryGcsBucket', self.temp_bucket)
+
+        return conf
+
+    def spark_context(self):
+        sc = SparkContext(conf=self.spark_conf)
+        return sc
+
+    def spark_sesion_builder(self):
+        spark = SparkSession.builder \
+            .appName('metar-project') \
+            .config(conf=self.spark_ctx.getConf())\
+            .getOrCreate()
+        return spark
+
+
+spark_manager = SparkManager(temp_bucket=temp_bucket)
+spark = spark_manager.spark
+
+
 schema = types.StructType([
     types.StructField("station", types.StringType(), True),
     types.StructField("valid", types.StringType(), True),
@@ -67,108 +90,85 @@ schema = types.StructType([
     types.StructField("snowdepth", types.DoubleType(), True)
 ])
 
-df = spark.read\
-    .schema(schema=schema)\
-    .parquet(input)
 
-df = df\
-    .withColumn('valid', to_timestamp(col('valid'), 'yyy-MM-dd HH:mm'))\
-    .replace(0.0, 360.0, "drct") \
-    .drop('p01i') 
+class DataTransformationManager:
+    def __init__(self,
+                 schema: types.StructType,
+                 spark: SparkSession,
+                 temp_table_name: str,
+                 sql_query,
+                 bq_output) -> None:
 
-df = df\
-    .withColumn("skyc1",when(df["skyc1"].isin("FEW", "OVC", "SCT", "BKN", "NSC", "VV"), df["skyc1"]) \
-    .otherwise(None))
-        
-df.registerTempTable('data_all')
-df.registerTempTable('data_wxcodes')
-df.registerTempTable('data_stats')
-df.registerTempTable('data_wind_drct')
-df.registerTempTable('data_skyc1')
+        self.spark = spark
+        self.schema = schema
+        self.temp_table_name = temp_table_name
+        self.sql_query = sql_query
+        self.data_frame = self.data_frame_writer()
+        self.data_frame_transformed = self.data_frame_transformations()
+        self.pyspark_query = self.sql_query_maker()
+        self.bq_output = bq_output
 
+    def data_frame_writer(self):
+        df = self.spark.read\
+            .schema(schema=self.schema)\
+            .parquet(input)
+        return df
 
-sql_data_all = spark.sql('''
-                 SELECT *,
-                 CONCAT(lat,", ", lon) AS GEOloc
-                 FROM data_all
-                 ''')
+    def data_frame_transformations(self):
+        df_transformed = self.data_frame\
+            .withColumn('valid', to_timestamp(col('valid'), 'yyy-MM-dd HH:mm'))\
+            .replace(0.0, 360.0, "drct") \
+            .drop('p01i') \
+            .withColumn("skyc1", when(self.data_frame["skyc1"].isin("FEW", "OVC", "SCT", "BKN", "NSC", "VV"), self.data_frame["skyc1"])
+                        .otherwise(None))
+        return df_transformed
 
-sql_wxcodes = spark.sql('''
-                SELECT station, COUNT(*) AS sum, wxcodes 
-                FROM data_wxcodes
-                WHERE wxcodes IS NOT NULL
-                GROUP BY station, wxcodes
-                ORDER BY station, sum DESC
-                ''')
+    def sql_query_maker(self):
+        self.data_frame_transformed \
+            .registerTempTable(self.temp_table_name)
 
-sql_stats = spark.sql('''
-                SELECT station, 
-                ROUND(AVG(sknt),2) AS avr_wind_speed,
-                ROUND(AVG((tmpf-32)/1.8),2) AS avg_air_temperature,
-                ROUND(MAX((tmpf-32)/1.8),2) AS max_air_temperature,
-                ROUND(MIN((tmpf-32)/1.8),2) AS min_air_temperature,
-                ROUND(AVG(dwpf-32/1.8),2) AS avg_dew_point,
-                ROUND(AVG(vsby),2) AS avg_visibility,
-                ROUND(AVG(drct)) AS avg_wind_direction,
-                ROUND(AVG(sknt),2) AS avg_wind_speed,
-                ROUND(MAX(gust)) AS max_gust,
-                ROUND(AVG(gust),2) AS avg_gust,
-                CONCAT(lat,", ", lon) AS GEOloc
-                FROM data_stats 
-                GROUP BY station, GEOloc
-                ''')
+        query = self.spark.sql(self.sql_query)
+        return query
 
-sql_wind_drct = spark.sql(f'''
-                SELECT station ,COUNT(*) AS direction_recorded, drct 
-                FROM data_wind_drct
-                WHERE drct is not null
-                GROUP BY station, drct
-                ORDER BY station, direction_recorded DESC
-                ''')
-
-sql_skyc1 = spark.sql(f'''
-                SELECT station, COUNT(*) AS sum, skyc1
-                FROM data_skyc1
-                WHERE skyc1 IS NOT NULL
-                GROUP BY station, skyc1
-                ORDER BY station, sum DESC
-                ''')
+    def bq_writer(self):
+        self.pyspark_query.write.format('bigquery') \
+            .option('table', self.bq_output + '_' + self.temp_table_name) \
+            .mode("overwrite") \
+            .save()
 
 
+bq_tables_to_create = {
+    "data_all":
+    '''
+    SELECT *,
+    CONCAT(lat,", ", lon) AS GEOloc
+    FROM data_all
+    ''',
 
-#sql_data_all.coalesce(1) \
-#    .write.parquet(gcs_output, mode='overwrite')
+    "data_wxcodes":
+    '''
+    SELECT station, COUNT(*) AS sum, wxcodes 
+    FROM data_wxcodes
+    WHERE wxcodes IS NOT NULL
+    GROUP BY station, wxcodes
+    ORDER BY station, sum DESC    
+    '''
+}
 
-# Use the Cloud Storage bucket for temporary BigQuery export data used
-# by the connector.
-bucket = temp_bucket
-spark.conf.set('temporaryGcsBucket', bucket)
 
-sql_data_all.write.format('bigquery') \
-    .option('table', bq_output) \
-    .mode("overwrite") \
-    .save()
-    
-sql_wxcodes.write.format('bigquery') \
-    .option('table', bq_output+'_wxcodes_')\
-    .mode("overwrite") \
-    .save()
-    
-sql_stats.write.format('bigquery') \
-    .option('table', bq_output+'_stats_')\
-    .mode("overwrite") \
-    .save()
-    
-sql_wind_drct.write.format('bigquery') \
-    .option('table', bq_output+'_wind_drct_')\
-    .mode("overwrite") \
-    .save()
-    
-sql_skyc1.write.format('bigquery') \
-    .option('table', bq_output+'_skyc1_')\
-    .mode("overwrite") \
-    .save()
-    
+def main(bq_tables_to_create):
+    for key, values in bq_tables_to_create.items():
 
-    
-#gsutil cp pyspark_sql.py gs://code112/pyspark_sql.py
+        temp_table_name = key
+        query = values
+
+        bq_table_create = DataTransformationManager(schema=schema,
+                                                    spark=spark,
+                                                    temp_table_name=temp_table_name,
+                                                    sql_query=query,
+                                                    bq_output=bq_output)
+        bq_table_create.bq_writer()
+
+
+if __name__ == '__main__':
+    main(bq_tables_to_create)
